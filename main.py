639409f5cbf1cc05a95f8220e884f994f77731b2
Uzair390-Del <uzair.zairi321@gmail.com>
2025-07-23 11:22:16 +0500

    updates

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import cv2
+from ultralytics import YOLO
+
+# reading an image using OPENCV
+image=cv2.imread("resources\images\image1.jpg")
+model =YOLO("yolo11n.pt")
+results=model(image,save=True,conf=0.80)
+# cv2.imshow("Image:",image)
+# cv2.waitKey(0)
+cv2.imshow()
